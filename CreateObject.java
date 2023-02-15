75f4912fed7a3b45ea4cf4c26897b3a45a685458
ashwani232001 <122799822+ashwani232001@users.noreply.github.com>
2023-02-16 00:05:35 +0530

    Create CreateObject.java

---
+++ CreateObject.java
@@ -0,0 +1,29 @@
+public class CreateObject {
+ 
+		// instance variables
+			  int a;
+			  int b;
+
+			  CreateObject(int aValue, int bValue) {
+			    a = aValue;
+			    b = bValue;
+			  }
+			  
+	          // Method
+			  int add() {
+			    return a + b;
+			  }
+			  
+			  public static void main(String[] args) {
+				  // Creating object
+				  CreateObject obj = new CreateObject(80, 30);
+			    int result = obj.add();
+
+			    System.out.println("Result: " + result);
+			  }
+			}
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privac
